554ab9ce2c062b4063010f180489e6e66d6e03ab
acis-iot <gtico1940@gmail.com>
2026-02-02 14:06:43 +0100

    Update vite.config.ts

--- temp/vite.config.ts
+++ temp/vite.config.ts
@@ -7,7 +7,6 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
 export default defineConfig({
-  base: '/gtico.github.io/',
   plugins: [react()],
   resolve: {
     alias: {
